1c5015f2c9640b5d74738d7cd9dc3b831c019475
Katarzyna Majchrzak <katarzyna.majchrzak1999@wp.pl>
2021-12-18 15:26:40 +0100

    Lab 9

---
+++ zad_1.py
@@ -0,0 +1,24 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+import cv2
+from os.path import exists
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
+
+def read_text_from_file(file_path: str):
+    if exists(file_path):
+        img = cv2.imread(file_path)
+        print(f'Extracted text from file {file_path}: \n {pytesseract.image_to_string(img)}\n')
+    else:
+        print(f'File {file_path} not found')
+
+
+read_text_from_file("photos/example1.jpg")
+read_text_from_file("photos/example2.jpg")
+read_text_from_file("photos/example3.png")
+read_text_from_file("photos/example4.jpg")
+read_text_from_file("photos/example5.jpg")
