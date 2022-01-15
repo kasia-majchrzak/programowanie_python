e860ef96549cdf5bb49eae20c40e61cb16868cc7
kasia-majchrzak <93868111+kasia-majchrzak@users.noreply.github.com>
2022-01-15 15:41:16 +0100

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from services.person_detector_service import detect
-from services.images_service import getImagesInDir
-
-images = getImagesInDir("images")
-for img in images:
-    img = detect(img)
